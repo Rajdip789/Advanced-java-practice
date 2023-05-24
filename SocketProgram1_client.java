c523021b1b1acaea6a49194a1979b57d6c4bebd7
rajdip <palrajdip33@gmail.com>
2023-05-24 21:04:05 +0530

    2nd commit

--- SocketProgram1_client.java
+++ SocketProgram1_client.java
@@ -1,17 +1,19 @@
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
-import java.awt.*;
-import java.awt.event.*;
+
 public class SocketProgram1_client{
     SocketProgram1_client() throws Exception{
         Socket s1 = new Socket("localhost", 7778);
+
+        System.out.println("Connection Established");
+
         Scanner ip = new Scanner(s1.getInputStream());
         PrintWriter op = new PrintWriter(s1.getOutputStream(), true);
         
-        op.println("Hello world");
         System.out.println(ip.nextLine());
-        //System.out.println(new Scanner(System.in).nextLine());
+        op.println("Hello world from client.......");
+        
         s1.close();
     }
     public static void main(String args[]) throws Exception{
--- SocketProgram1_server.java
+++ SocketProgram1_server.java
@@ -1,20 +1,20 @@
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
-import java.awt.*;
-import java.awt.event.*;
+
 public class SocketProgram1_server{
     SocketProgram1_server() throws Exception{
         ServerSocket sk1 = new ServerSocket(7778);
         Socket s1 = sk1.accept();
 
         System.out.println("Connection Established");
+
         Scanner ip = new Scanner(s1.getInputStream());
         PrintWriter op = new PrintWriter(s1.getOutputStream(), true);
 
-        System.out.println(ip.nextLine());
-        op.println("Bye world");
-        System.out.println(new Scanner(System.in).nextLine());
+        op.println("Hello world from server.......");
+        System.out.println(ip.nextLine()); 
+
         s1.close();
         System.out.println(s1.isClosed());
     }
